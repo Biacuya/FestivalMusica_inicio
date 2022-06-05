1ce2b34e28036e0424eb19e24b2793c164f33d97
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2022-06-05 11:58:34 -0500

    Creation of diferents extencions for img

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+//Imagenes
+const cache = require("gulp-cache");
+const imageMin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(callback) {
     src("src/scss/**/*.scss") //Identificar el archivo SASS
@@ -11,11 +19,46 @@ function css(callback) {
     callback();//Avisa al gulp que llegamos al final
 }
 
-function dev(callback) {
-    watch("src/scss/**/*.scss", css);
+function imagenes(callback) {
+    const options = {
+        optimizationLevel: 3
+    };
+
+    src("src/img/**/*.{jpg,png}")
+        .pipe(cache(imageMin(options)))
+        .pipe(dest("build/img"))
+    callback();
+}
+
+function versionWebp(callback) {
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{jpg,png}")
+        .pipe(webp(options))
+        .pipe(dest("build/img"))
 
     callback();
 }
 
+function versionAvif(callback) {
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{jpg,png}")
+        .pipe(avif(options))
+        .pipe(dest("build/img"))
+
+    callback();
+}
+
+function dev(callback) {
+    watch("src/scss/**/*.scss", css);
+    callback();
+}
+
 exports.css = css; //exports es codigo node
-exports.dev = dev
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev); //parallel ejecuta todas las funciones al mismo tiempo
